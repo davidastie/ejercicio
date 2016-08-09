c0dc3c33c7611ba566f5cb5529584a33093c9195
davidastie <davo_43@hotmail.com>
2016-08-08 20:35:58 -0500

    version nueva

---
+++ Prueba+Holamundo.py
@@ -0,0 +1,100 @@
+
+# coding: utf-8
+
+# In[3]:
+
+print ("hola mundo")
+
+
+# $$\sum_{p}li(x^p)-log(2)+\int_x^\infty\frac{dt}{t(t^2-1)log(t)}$$
+# 
+
+# In[5]:
+
+2+4*6/8.0-3
+
+
+# In[6]:
+
+3/2
+
+
+# In[7]:
+
+8/0
+
+
+# # HOLA MUNDO
+
+# In[11]:
+
+3+5j
+
+
+# In[13]:
+
+(3+5j)-(1-1j)
+
+
+# In[14]:
+
+abs(3+5j)
+
+
+# In[15]:
+
+5**3
+
+
+# In[16]:
+
+int(_14)
+
+
+# In[18]:
+
+float(_15)
+
+
+# In[19]:
+
+complex(_5)
+
+
+# In[24]:
+
+round(_14,2)
+
+
+# In[22]:
+
+max(1,3,-5)
+
+
+# In[25]:
+
+min(8,10,20,30)
+
+
+# In[27]:
+
+x,y,z,a,b=0,1,2,3,4
+
+
+# In[32]:
+
+print(z); print(a); print(y); print(x); print(b)
+
+
+# In[34]:
+
+print(z+b)
+
+
+# In[45]:
+
+if x>y:
+    print("El numero mayor es",x)
+else:
+    print("El numero menor es",y)
+
